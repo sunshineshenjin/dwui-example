e5bba1b69eb42d96da7f50a25532375b4db78dfe
tony shen <2434685038@qq.com>
2018-12-07 16:51:35 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app"><baseTable :rowData="tableData" :columns="columns" /></div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import BaseTable from "dwui/packages/base-table/base-table";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    BaseTable
+  },
+  data() {
+    return {
+      columns: [
+        { title: "ID", key: "id", sortable: true, width: 70 },
+        { title: "唯一标识", key: "account" },
+        { title: "产品名", key: "productName" },
+        { title: "类型", key: "typeName" },
+        { title: "平台", key: "platformName" },
+        { title: "域名", key: "domain" },
+        { title: "补充说明", key: "summary" },
+        { title: "创建时间", key: "createTime" },
+        { title: "更新时间", key: "updateTime" }
+      ],
+      tableData: []
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+import iView from "iview";
+import "iview/dist/styles/iview.css";
+import dwui from "dwui";
+
+Vue.config.productionTip = false;
+Vue.use(iView, {
+  transfer: true,
+  size: "default"
+});
+Vue.use(dwui);
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>"
+});
